1ad66e4daa7fcd35e6212a91fa50ad1dc45bf77f
Simon Courtecuisse <simoncourtecuisse@gmail.com>
2022-07-19 16:47:29 +0200

    javadoc

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/PoseidonApp.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/PoseidonApp.java
@@ -3,6 +3,12 @@ package com.nnk.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Poseidon Inc. Application.
+ *
+ * @author SimonC.
+ * @version 1.0
+ */
 @SpringBootApplication
 public class PoseidonApp {
 
